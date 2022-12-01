541e5313e00aa3b18bc25e12fc51ee86ade09fba
Biendeo <biendeo@biendeo.com>
2022-12-01 17:50:35 +1100

    Cleaned up day 1 and added a benchmark feature to the program.

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,85 +1,147 @@
 ﻿using AdventOfCodeLib;
 using System.Diagnostics;
 
-static ChallengeResult RunSolution(IDayChallenge dayChallenge, int part, string[] inputLines) {
-	Stopwatch stopwatch = new();
-	stopwatch.Start();
-	string result = part == 1 ? dayChallenge.PartOneFromInput(inputLines) : dayChallenge.PartTwoFromInput(inputLines);
-	stopwatch.Stop();
-	return new ChallengeResult(dayChallenge, part, result, stopwatch.ElapsedTicks);
-}
-
-static IDayChallenge[] GetAllDayChallenges() => typeof(IDayChallenge).Assembly.GetTypes()
-	.Where(t => t.GetInterfaces().Contains(typeof(IDayChallenge)))
-	.Select(t => (IDayChallenge)t.GetConstructors().Single().Invoke(Array.Empty<object>()))
-	.ToArray();
-
-static string[] GetDayInput(HttpClient httpClient, int day) {
-	if (!Directory.Exists("input")) {
-		Directory.CreateDirectory("input");
-	}
-	string expectedFilePath = Path.Join("input", $"{day}.txt");
-	if (!File.Exists(expectedFilePath)) {
-		string[] inputLines = httpClient.GetStringAsync($"/2022/day/{day}/input").Result.Trim().Split('\n');
-		File.WriteAllLines(expectedFilePath, inputLines);
-		return inputLines;
-	} else {
-		return File.ReadAllLines(expectedFilePath);
-	}
-}
-
-string? session = Environment.GetEnvironmentVariable("AdventOfCodeSession");
-if (session == null) {
-	Console.Error.WriteLine("Please set the \"AdventOfCodeSession\" environment variable from your cookie");
-	Environment.Exit(1);
-}
-
-IDayChallenge[] dayChallenges = GetAllDayChallenges();
-
-List<Func<ChallengeResult>> singleThreadedChallenges = new();
-List<Func<ChallengeResult>> multiThreadedChallenges = new();
-
-using (HttpClient httpClient = new()) {
-	httpClient.BaseAddress = new(@"https://adventofcode.com/");
-	httpClient.DefaultRequestHeaders.Add("cookie", $"session={session}");
-
-	foreach (IDayChallenge dayChallenge in dayChallenges.Where(dc => !dc.IsIgnored)) {
-		Console.WriteLine($"Getting input for day {dayChallenge.Day}...");
-		string[] inputLines = GetDayInput(httpClient, dayChallenge.Day);
-		ChallengeResult partOneTask() => RunSolution(dayChallenge, 1, inputLines);
-		if (dayChallenge.IsPartOneSingleThreaded) {
-			singleThreadedChallenges.Add(partOneTask);
-		} else {
-			multiThreadedChallenges.Add(partOneTask);
+internal class Program {
+	public static void Main(string[] args) {
+		string? session = Environment.GetEnvironmentVariable("AdventOfCodeSession");
+		if (session == null) {
+			Console.Error.WriteLine("Please set the \"AdventOfCodeSession\" environment variable from your cookie");
+			Environment.Exit(1);
 		}
-		ChallengeResult partTwoTask() => RunSolution(dayChallenge, 2, inputLines);
-		if (dayChallenge.IsPartTwoSingleThreaded) {
-			singleThreadedChallenges.Add(partTwoTask);
+
+		if (args.Contains("--benchmark")) {
+			RunAllBenchmarks(session, int.Parse(args[Array.IndexOf(args, "--benchmark") + 1]));
 		} else {
-			multiThreadedChallenges.Add(partTwoTask);
+			RunAllChallenges(session);
 		}
 	}
+
+	static ChallengeResult RunSolution(IDayChallenge dayChallenge, int part, string[] inputLines) {
+		Stopwatch stopwatch = new();
+		stopwatch.Start();
+		string result = part == 1 ? dayChallenge.PartOneFromInput(inputLines) : dayChallenge.PartTwoFromInput(inputLines);
+		stopwatch.Stop();
+		return new ChallengeResult(dayChallenge, part, result, stopwatch.ElapsedTicks);
+	}
+
+	static BenchmarkResult BenchmarkSolution(IDayChallenge dayChallenge, int part, string[] inputLines, int iterations) {
+		long totalTicks = 0L;
+		Enumerable.Range(0, iterations).AsParallel().ForAll((_) => {
+			Stopwatch stopwatch = new();
+			stopwatch.Start();
+			if (part == 1)
+				dayChallenge.PartOneFromInput(inputLines);
+			else
+				dayChallenge.PartTwoFromInput(inputLines);
+			stopwatch.Stop();
+			Interlocked.Add(ref totalTicks, stopwatch.ElapsedTicks);
+		});
+		return new BenchmarkResult(dayChallenge, part, totalTicks, iterations);
+	}
+
+	static IDayChallenge[] GetAllDayChallenges() => typeof(IDayChallenge).Assembly.GetTypes()
+		.Where(t => t.GetInterfaces().Contains(typeof(IDayChallenge)))
+		.Select(t => (IDayChallenge)t.GetConstructors().Single().Invoke(Array.Empty<object>()))
+		.ToArray();
+
+	static HttpClient GetAdventOfCodeClient(string session) {
+		using HttpClient httpClient = new();
+		httpClient.BaseAddress = new(@"https://adventofcode.com/");
+		httpClient.DefaultRequestHeaders.Add("cookie", $"session={session}");
+		return httpClient;
+	}
+
+	static string[] GetDayInput(HttpClient httpClient, int day) {
+		if (!Directory.Exists("input")) {
+			Directory.CreateDirectory("input");
+		}
+		string expectedFilePath = Path.Join("input", $"{day}.txt");
+		if (!File.Exists(expectedFilePath)) {
+			string[] inputLines = httpClient.GetStringAsync($"/2022/day/{day}/input").Result.Trim().Split('\n');
+			File.WriteAllLines(expectedFilePath, inputLines);
+			return inputLines;
+		} else {
+			return File.ReadAllLines(expectedFilePath);
+		}
+	}
+
+	static (List<Func<ChallengeResult>> singleThreadedChallenges, List<Func<ChallengeResult>> multiThreadedChallenges) AggregateChallengesByThreads(string session) {
+		IDayChallenge[] dayChallenges = GetAllDayChallenges();
+
+		List<Func<ChallengeResult>> singleThreadedChallenges = new();
+		List<Func<ChallengeResult>> multiThreadedChallenges = new();
+
+		using HttpClient httpClient = GetAdventOfCodeClient(session);
+
+		foreach (IDayChallenge dayChallenge in dayChallenges.Where(dc => !dc.IsIgnored)) {
+			Console.WriteLine($"Getting input for day {dayChallenge.Day}...");
+			string[] inputLines = GetDayInput(httpClient, dayChallenge.Day);
+			ChallengeResult partOneTask() => RunSolution(dayChallenge, 1, inputLines);
+			if (dayChallenge.IsPartOneSingleThreaded) {
+				singleThreadedChallenges.Add(partOneTask);
+			} else {
+				multiThreadedChallenges.Add(partOneTask);
+			}
+			ChallengeResult partTwoTask() => RunSolution(dayChallenge, 2, inputLines);
+			if (dayChallenge.IsPartTwoSingleThreaded) {
+				singleThreadedChallenges.Add(partTwoTask);
+			} else {
+				multiThreadedChallenges.Add(partTwoTask);
+			}
+		}
+
+		return (singleThreadedChallenges, multiThreadedChallenges);
+	}
+
+	static void RunAllChallenges(string session) {
+		(List<Func<ChallengeResult>> singleThreadedChallenges, List<Func<ChallengeResult>> multiThreadedChallenges) = AggregateChallengesByThreads(session);
+
+		Console.WriteLine($"Running challenges...");
+		Console.WriteLine();
+
+		Stopwatch stopwatch = new();
+		stopwatch.Start();
+		IEnumerable<ChallengeResult> singleThreadedResults = singleThreadedChallenges.AsParallel().Select(f => f());
+		IEnumerable<ChallengeResult> multiThreadedResults = multiThreadedChallenges.Select(f => f());
+		List<ChallengeResult> allResults = singleThreadedResults.Concat(multiThreadedResults).ToList();
+		stopwatch.Stop();
+
+		foreach (ChallengeResult challengeResult in allResults.OrderBy(r => r.DayChallenge.Day * 2 + r.Part)) {
+			Console.WriteLine($"""
+				Day {challengeResult.DayChallenge.Day} - Part {challengeResult.Part} - {challengeResult.DayChallenge.Name}
+				{challengeResult.Result}
+				Completed in {challengeResult.Ticks / (Stopwatch.Frequency / 1000000)}μs
+
+				""");
+		}
+
+		Console.WriteLine($"Total time {stopwatch.ElapsedTicks / (Stopwatch.Frequency / 1000000)}μs");
+	}
+
+	static void RunAllBenchmarks(string session, int iterations) {
+		IDayChallenge[] dayChallenges = GetAllDayChallenges();
+
+		Console.WriteLine($"Running benchmark...");
+		Console.WriteLine();
+
+		using HttpClient httpClient = GetAdventOfCodeClient(session);
+
+		foreach (IDayChallenge dayChallenge in dayChallenges) {
+			Console.WriteLine($"Getting input for day {dayChallenge.Day}...");
+			string[] inputLines = GetDayInput(httpClient, dayChallenge.Day);
+
+			for (int i = 1; i <= 2; ++i) {
+				Console.WriteLine($"Day {dayChallenge.Day} - Part {i} - {dayChallenge.Name}");
+				BenchmarkResult benchmarkResult = BenchmarkSolution(dayChallenge, i, inputLines, iterations);
+				Console.WriteLine($"""
+					Completed {benchmarkResult.Iterations} in {benchmarkResult.Ticks / (Stopwatch.Frequency / 1000000)}μs
+					Averaged {benchmarkResult.Ticks / (Stopwatch.Frequency / 1000000.0) / benchmarkResult.Iterations:0.00000}μs
+				
+					""");
+			}
+		}
+	}
 }
-
-Console.WriteLine($"Running challenges...");
-Console.WriteLine();
-
-Stopwatch stopwatch = new();
-stopwatch.Start();
-IEnumerable<ChallengeResult> singleThreadedResults = singleThreadedChallenges.AsParallel().Select(f => f());
-IEnumerable<ChallengeResult> multiThreadedResults = multiThreadedChallenges.Select(f => f());
-List<ChallengeResult> allResults = singleThreadedResults.Concat(multiThreadedResults).ToList();
-stopwatch.Stop();
-
-foreach (ChallengeResult challengeResult in allResults.OrderBy(r => r.DayChallenge.Day * 2 + r.Part)) {
-	Console.WriteLine($"""
-		Day {challengeResult.DayChallenge.Day} - Part {challengeResult.Part} - {challengeResult.DayChallenge.Name}
-		{challengeResult.Result}
-		Completed in {challengeResult.Ticks / (Stopwatch.Frequency / 1000000)}μs
-
-		""");
-}
-
-Console.WriteLine($"Total time {stopwatch.ElapsedTicks / (Stopwatch.Frequency / 1000000)}μs");
 
 internal record ChallengeResult(IDayChallenge DayChallenge, int Part, string Result, long Ticks);
+internal record BenchmarkResult(IDayChallenge DayChallenge, int Part, long Ticks, int Iterations);
--- AdventOfCodeLib/Challenges/Day01.cs
+++ AdventOfCodeLib/Challenges/Day01.cs
@@ -4,22 +4,22 @@
 public class Day01 : IDayChallenge {
 	public string PartOneFromInput(string[] inputLines) => PartOne(inputLines).ToString();
 
-	public int PartOne(string[] calories) => GetElfCaloriesFromInput(calories).Max(e => e.Sum());
+	public int PartOne(string[] calories) => GetElfCaloriesFromInput(calories).Max();
 
 	public string PartTwoFromInput(string[] inputLines) => PartTwo(inputLines).ToString();
 
-	public int PartTwo(string[] calories) => GetElfCaloriesFromInput(calories).Select(e => e.Sum()).OrderByDescending(x => x).Take(3).Sum();
+	public int PartTwo(string[] calories) => GetElfCaloriesFromInput(calories).OrderByDescending(x => x).Take(3).Sum();
 
-	private List<List<int>> GetElfCaloriesFromInput(string[] calories) {
-		List<List<int>> elfCalories = new();
-		List<int> currentElf = new();
+	private List<int> GetElfCaloriesFromInput(string[] calories) {
+		List<int> elfCalories = new();
+		int currentElf = 0;
 
 		foreach (string calorieReading in calories) {
 			if (string.IsNullOrEmpty(calorieReading)) {
 				elfCalories.Add(currentElf);
-				currentElf = new();
+				currentElf = 0;
 			} else {
-				currentElf.Add(int.Parse(calorieReading));
+				currentElf += int.Parse(calorieReading);
 			}
 		}
 
