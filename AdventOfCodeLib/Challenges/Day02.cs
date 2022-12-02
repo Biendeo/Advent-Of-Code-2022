592e287bb000018aa0dbb006404f335f25e91827
Biendeo <biendeo@biendeo.com>
2022-12-02 16:30:17 +1100

    Modified Day02 to use switch expressions.

--- AdventOfCodeLib/Challenges/Day02.cs
+++ AdventOfCodeLib/Challenges/Day02.cs
@@ -5,30 +5,21 @@ public class Day02 : IDayChallenge {
 	public string PartOneFromInput(string[] inputLines) => PartOne(inputLines).ToString();
 
 	public int PartOne(string[] strategyGuide) => strategyGuide.Sum(game => {
-		Result result;
-		int opponent = game[0] - 'A';
-		int player = game[2] - 'X';
-		if (opponent == player) {
-			result = Result.Draw;
-		} else if (player - opponent == 1 || player - opponent == -2) {
-			result = Result.Win;
-		} else {
-			result = Result.Loss;
-		}
-		return (result == Result.Win ? 6 : (result == Result.Draw ? 3 : 0)) + player + 1;
+		return ((game[2] - game[0] + 4) % 3) switch {
+			0 => 3,
+			1 => 6,
+			_ => 0
+		} + game[2] - 'W';
 	});
 
 	public string PartTwoFromInput(string[] inputLines) => PartTwo(inputLines).ToString();
 
 	public int PartTwo(string[] strategyGuide) => strategyGuide.Sum(game => {
 		int opponent = game[0] - 'A';
-		Result result = (Result)(game[2] - 'X');
-		return (result == Result.Win ? 6 + ((opponent + 1) % 3) : (result == Result.Draw ? 3 + (opponent % 3) : ((opponent + 2) % 3))) + 1;
+		return (game[2] - 'X') switch {
+			2 => 6 + ((opponent + 1) % 3),
+			1 => 3 + (opponent % 3),
+			_ => (opponent + 2) % 3,
+		} + 1;
 	});
-
-	private enum Result {
-		Loss,
-		Draw,
-		Win
-	}
 }
