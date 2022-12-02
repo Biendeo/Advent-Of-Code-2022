55c844f31f409d00b211648b3af6b5926d708496
Biendeo <biendeo@biendeo.com>
2022-12-02 16:39:11 +1100

    One last cleanup, reduced unecessary additions.

--- AdventOfCodeLib/Challenges/Day02.cs
+++ AdventOfCodeLib/Challenges/Day02.cs
@@ -5,7 +5,7 @@ public class Day02 : IDayChallenge {
 	public string PartOneFromInput(string[] inputLines) => PartOne(inputLines).ToString();
 
 	public int PartOne(string[] strategyGuide) => strategyGuide.Sum(game => {
-		return ((game[2] - game[0] + 4) % 3) switch {
+		return ((game[2] - game[0] + 1) % 3) switch {
 			0 => 3,
 			1 => 6,
 			_ => 0
@@ -15,11 +15,10 @@ public class Day02 : IDayChallenge {
 	public string PartTwoFromInput(string[] inputLines) => PartTwo(inputLines).ToString();
 
 	public int PartTwo(string[] strategyGuide) => strategyGuide.Sum(game => {
-		int opponent = game[0] - 'A';
-		return (game[2] - 'X') switch {
-			2 => 6 + ((opponent + 1) % 3),
-			1 => 3 + (opponent % 3),
-			_ => (opponent + 2) % 3,
+		return game[2] switch {
+			'Z' => ((game[0] + 2) % 3) + 6,
+			'Y' => ((game[0] + 1) % 3) + 3,
+			_   => game[0] % 3,
 		} + 1;
 	});
 }
